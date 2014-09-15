7dc116c58ce43ef9abaa6ced14f202f9661242ab
spirilis <spirilis@spirilis.net>
2014-09-15 15:25:58 -0400

    Copyright / license notice

--- Franklin.cpp
+++ Franklin.cpp
@@ -2,6 +2,20 @@
  * Written for Energia, compatible with Arduino except for the calibration examples.
  * Calibration examples designed for the TI MSP430 LaunchPad platforms using the
  *   MSP430's internal timer architecture.
+ *
+ *
+ * Copyright (c) 2014, Eric Brundick <spirilis@linux.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose
+ * with or without fee is hereby granted, provided that the above copyright notice
+ * and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT,
+ * OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
+ * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 #include "Franklin.h"
